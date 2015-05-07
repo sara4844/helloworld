3c1185e87fb686b634490e56d3a7fa0a5ebb230f
Katura Harvey <katuraharvey@gmail.com>
2015-05-07 17:19:19 -0400

    more final version

--- src/bank/bank.c
+++ src/bank/bank.c
@@ -18,7 +18,7 @@ Bank* bank_create(char *filename)
 	
 	FILE *bank_file = fopen(filename, "rb"); 
 	if (bank_file == NULL){
-		printf("Error opening Bank initialization file\n");
+		printf("Error opening bank initialization file\n");
 		exit(64);
 	}
 	
@@ -126,7 +126,7 @@ void bank_process_local_command(Bank *bank, char *command, size_t len)
 		
 		//check if user exists
 		if (hash_table_find(bank->users, username) != NULL){
-			printf("Error: user %s alread exists\n", username);
+			printf("Error: user %s already exists\n", username);
 			return;
 		}
 		
